1f3f1fbaffde3b128ae2d443f0679abd267ca4d4
dkoe00 <darius.koenig@outlook.de>
2025-03-09 16:08:49 +0100

    Add download flag

--- ballroom_final.py
+++ ballroom_final.py
@@ -2,7 +2,7 @@ import argparse
 
 
 def main():
-   level, song_length, pause_length = parse_arguments()
+   download, level, song_length, pause_length = parse_arguments()
 
 
 def parse_arguments():
@@ -10,6 +10,7 @@ def parse_arguments():
     
     # create a list of given arguments
     parser = argparse.ArgumentParser(description="Play ballroom final music")
+    parser.add_argument("--download", "-d", help="Download all songs from playlists first", action="store_true")
     parser.add_argument("--klasse", "-k", help="Specify the class of the final (d, c, b for b and up)", type=str)
     parser.add_argument("--length", "-l", help="Specify length of songs (long for 1:50-2:10, normal for 1:30-1:50)", type=str)
     parser.add_argument("--pause", "-p", help="Specify pause time between titles in seconds", type=int)
@@ -17,11 +18,12 @@ def parse_arguments():
     args = parser.parse_args()
 
     # handle optional arguments
+    download = args.download
     level = args.klasse if args.klasse else "b"
     song_length = args.length if args.length else "normal"
     pause_length = args.pause if args.pause else 30
 
-    return level, song_length, pause_length
+    return download, level, song_length, pause_length
 
 
 if __name__ == "__main__":
